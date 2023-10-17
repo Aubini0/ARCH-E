e9904f3b451caf80b736ea758b22ca4c72adf569
EmekaOkonkwo101 <okonkwoemeka1000@gmail.com>
2023-10-17 21:29:06 +0100

    Initialize repo

---
+++ db/connectDB.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://hycienth:oYnyFCGMSusIvxZO@amplifi.m87n5am.mongodb.net/?retryWrites=true&w=majority",
+      {
+        // To avoid warnings in the console
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
