f96ec309a1a0b87e3ab07f515731b3fd1b4a3b99
EmekaOkonkwo101 <okonkwoemeka1000@gmail.com>
2023-10-20 21:46:45 +0100

    reimplement server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 2000;
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
