a2ac8eba5bacddcb126e7ffd310aebfcfc1ffca5
Alabi Damilola Daniel <eminencemailx@gmail.com>
2023-10-25 02:10:52 -0700

    updated  create  audio post  api

--- controllers/userController.js
+++ controllers/userController.js
@@ -30,11 +30,7 @@ const getUserProfile = async(req, res) => {
     }
     res.status(200).json(user);
 }
-}
-catch (err) {
-    res.status(500).json({ error: err.message });
-}
-};
+
 
 const signupUser = async(req, res) => {
     try {
@@ -88,7 +84,7 @@ const loginUser = async(req, res) => {
     try {
         const { username, password } = req.body;
         const user = await User.findOne({ username });
-        const isPasswordCorrect = await bcrypt.compare(password, user ? .password || "");
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
         if (!user || !isPasswordCorrect) return res.status(400).json({ error: "Invalid username or password" });
 
