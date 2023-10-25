452bef96a1f96c160d447fb5e234f8ae1415dfcc
EmekaOkonkwo101 <okonkwoemeka1000@gmail.com>
2023-10-25 13:17:20 +0100

    ci/cd implementation

--- controllers/userController.js
+++ controllers/userController.js
@@ -29,11 +29,6 @@ const getUserProfile = async(req, res) => {
         console.log("Error in getUserProfile: ", err.message);
     }
     res.status(200).json(user);
-}
-}
-catch (err) {
-    res.status(500).json({ error: err.message });
-}
 };
 
 const signupUser = async(req, res) => {
@@ -88,7 +83,7 @@ const loginUser = async(req, res) => {
     try {
         const { username, password } = req.body;
         const user = await User.findOne({ username });
-        const isPasswordCorrect = await bcrypt.compare(password, user ? .password || "");
+        const isPasswordCorrect = await bcrypt.compare(password, user.password || "");
 
         if (!user || !isPasswordCorrect) return res.status(400).json({ error: "Invalid username or password" });
 
@@ -96,6 +91,7 @@ const loginUser = async(req, res) => {
             user.isFrozen = false;
             await user.save();
         }
+
 
         generateTokenAndSetCookie(user._id, res);
 
