ceeca1c08ca0f02a07d28b0194c40aba398dbcab
EmekaOkonkwo101 <okonkwoemeka1000@gmail.com>
2023-10-25 23:53:39 +0100

    ci/cd implementation

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(cors());
 
-const PORT = process.env.PORT || 6000;
+const PORT = process.env.PORT || 8080;
 
 // cloudinary.config({
 //   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
