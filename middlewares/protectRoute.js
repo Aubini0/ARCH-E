1e6a6d8eb5b20384e6b00218a3b97428ca38c726
Alabi Damilola Daniel <eminencemailx@gmail.com>
2023-10-27 02:49:18 -0700

    createe and get post endpoint created

--- middlewares/protectRoute.js
+++ middlewares/protectRoute.js
@@ -4,6 +4,7 @@ import jwt from "jsonwebtoken";
 const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt || req.headers["authorization"]
+    console.log(token)
     if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, "Hycienth");
