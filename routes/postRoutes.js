aacf7082116069d9fb3b23b23664da355f5cbd78
saint <hmichaelsintayehu@gmail.com>
2023-11-03 16:40:00 +0300

    make the upvote/downvote components protected

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -26,7 +26,7 @@ router.delete("/:id", protectRoute, deletePost);
 router.put("/like/:id", protectRoute, likeUnlikePost);
 router.put("/reply/:id", protectRoute, replyToPost);
 router.delete("/reply/delete/:id/:replyId", protectRoute, deleteComment);
-router.put('/upvote/:postId/:userId', upvotePost);
-router.put('/downvote/:postId/:userId', downvotePost);
+router.put('/upvote/:postId/:userId', protectRoute ,upvotePost);
+router.put('/downvote/:postId/:userId', protectRoute ,downvotePost);
 
 export default router;
