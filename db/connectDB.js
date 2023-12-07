83bc74b22d55ab3c60fe6ab2de3a57b257a52446
EmekaOkonkwo101 <okonkwoemeka1000@gmail.com>
2023-12-07 21:06:45 +0100

    add babbl signup

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,10 +3,9 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect(
-            `mongodb+srv://${process.env.USERMONGO}:${process.env.PASSWORDMONGO}@amplifi.m87n5am.mongodb.net/?retryWrites=true&w=majority`, 
+            `mongodb+srv://${process.env.USERMONGO}:${process.env.PASSWORDMONGO}@amplifi.m87n5am.mongodb.net/?retryWrites=true&w=majority`,
             // `mongodb://localhost:27017/Amplififeed`,
             // `mongodb+srv://${process.env.USERMONGO}:${process.env.PASSWORDMONGO}@amplifieddb.ksrtmte.mongodb.net/`,
-            
             {
                 // To avoid warnings in the console
                 useNewUrlParser: true,
