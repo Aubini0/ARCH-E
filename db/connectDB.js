ded7f9d54ed953115d55ef870d2e801b5d90a347
Muhammad Abdullah <abdullahquddus7@gmail.com>
2024-01-08 00:03:30 +0500

    typo issue resolved

--- db/connectDB.js
+++ db/connectDB.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async() => {
     try {
         const conn = await mongoose.connect(
-            `mongodb+srv://${process.env.USERMONGO}:${pclsrocess.env.PASSWORDMONGO}@amplifi.m87n5am.mongodb.net/?retryWrites=true&w=majority`,
+            `mongodb+srv://${process.env.USERMONGO}:${process.env.PASSWORDMONGO}@amplifi.m87n5am.mongodb.net/?retryWrites=true&w=majority`,
             // `mongodb://localhost:27017/Amplififeed`,
             // `mongodb+srv://${process.env.USERMONGO}:${process.env.PASSWORDMONGO}@amplifieddb.ksrtmte.mongodb.net/`,
             {
